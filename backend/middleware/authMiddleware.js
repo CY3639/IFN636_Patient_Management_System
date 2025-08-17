166be7352425d492609b14215b372a529f220e13
ChiChi <162544938+CY3639@users.noreply.github.com>
2025-08-17 22:34:02 +1000

    ip address

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -12,12 +12,12 @@ const protect = async (req, res, next) => {
             req.user = await User.findById(decoded.id).select('-password');
             next();
         } catch (error) {
-            res.status(401).json({ message: 'Not authorized, token failed' });
+            res.status(401).json({ message: 'Not authorised, token failed' });
         }
     }
 
     if (!token) {
-        res.status(401).json({ message: 'Not authorized, no token' });
+        res.status(401).json({ message: 'Not authorised, no token' });
     }
 };
 
