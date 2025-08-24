ecdd3b03f2d668053945dd9736b5158f0fdd8f9a
ChiChi <162544938+CY3639@users.noreply.github.com>
2025-08-24 15:38:12 +1000

    update live ip address

--- frontend/src/axiosConfig.jsx
+++ frontend/src/axiosConfig.jsx
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const axiosInstance = axios.create({
   //baseURL: 'http://localhost:5001', // local
-  baseURL: 'http://54.206.126.82:5001', // live
+  baseURL: 'http://3.27.185.225:5001', // live
   headers: { 'Content-Type': 'application/json' },
 });
 
